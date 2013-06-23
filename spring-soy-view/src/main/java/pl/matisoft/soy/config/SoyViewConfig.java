77af0651d10d3308ac51a6892f2777a464001b8d
Mateusz Szczap <mateusz.szczap@gmail.com>
2013-06-23 18:13:08 +0200

    introduced debugOn in one class config (cross cutting config)

---
+++ spring-soy-view/src/main/java/pl/matisoft/soy/config/SoyViewConfig.java
@@ -0,0 +1,21 @@
+package pl.matisoft.soy.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mati
+ * Date: 23/06/2013
+ * Time: 18:02
+ */
+public class SoyViewConfig {
+
+    private boolean debugOn;
+
+    public boolean isDebugOn() {
+        return debugOn;
+    }
+
+    public void setDebugOn(final boolean debugOn) {
+        this.debugOn = debugOn;
+    }
+
+}
